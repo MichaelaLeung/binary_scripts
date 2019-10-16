831275b3bc03531953f5305534afad4ebe29e225
Michaela Leung <mwl@sauria.com>
2019-10-15 18:13:13 -0700

    adding weights

--- mixing_ratio_plot.py
+++ mixing_ratio_plot.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
                                rm_after_submit = True)
     elif platform.node().startswith("n"):
         # On a mox compute node: ready to run
-        run_plots([1000,2000,3000,4000,5000,6000,7000,8000,9000,10000], "GG")
+        run_plots([10000,20000,30000,40000,50000,60000,70000,80000,90000,100000], "GG")
     else:
         plot_mixingratios('GG_output_pt.txt', 5)
 
