2b1af35a937988216112cf6f6b2511b7b8d75170
Michaela Leung <mwl@sauria.com>
2019-11-08 10:46:37 -0800

    normalized integration

--- UVband_Integ.py
+++ UVband_Integ.py
@@ -11,7 +11,6 @@ import math
 import csv
 
 from spectral_weights import smart_spectral_integ
-print("imports")
 
 def integrate(pair, band, h):
     print("integrate")
@@ -60,6 +59,64 @@ def integrate(pair, band, h):
     f.write(str(name) + "\n")
  #   return(pair, band, i)
 
+def integrate_norm(pair, band, h):
+    if band == 'a':
+        lamin = 0.315
+        lamax = 0.40
+    elif band == 'b':
+        lamin = 0.28
+        lamax = 0.315
+    elif band == 'c':
+        lamin = 0.10
+        lamax = 0.28
+        
+    f = open("/gscratch/vsm/mwjl/projects/binary/scripts/integrations.txt", "a")
+    wl, flux = run_smart_toa(lamin, lamax, 0.01)
+    wl_low, flux_low = run_smart_toa(lamin,lamax, 1)
+    
+    bi_wl, bi_flux = smart_spectral_integ(pair, band, h, 0.01)
+    bi_wl_low, bi_flux_low = smart_spectral_integ(pair, band, h, 1)
+
+    long_flux = []
+    for i in flux_low:
+        j = 0
+        while j < 100: 
+            long_flux.append(i)
+            j = j+1
+
+    mixed = []
+    i = 0
+    while i < len(flux):
+        temp = (flux[i] + long_flux[i]) / 2
+        mixed.append(temp)
+        i = i+1
+
+
+    i = 0
+    flattened = []
+    while i < len(long_flux)- 25: 
+        avg = np.mean(flux[i:i+25])
+        j = 0
+        while j < 25:
+            flattened.append(avg)
+            j = j+1
+        i = i+25
+        
+
+    out = []
+    i = 0
+    while i < len(mixed[:-25]): 
+        diff = abs(mixed[i] - flattened[i])
+        out.append(diff)
+        i = i+1
+
+    import scipy.integrate as integrate
+    adds = integrate.trapz(out, wl[:-25])
+    print(adds)    
+    name = str(abs(adds)), str(i), str(pair)
+    f = open("/gscratch/vsm/mwjl/projects/binary/scripts/integrations_norm.txt", "a")
+    f.write(str(name) + "\n")
+    
 def output(pair,i):
     print("output")
     integrate(pair, 'a', i)
