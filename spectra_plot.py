e1c0b075cf691ba47b74489707e79d19bc4d1225
M. Leung <mwjl@mox2.hyak.local>
2020-06-05 12:33:48 -0700

    moving to quad panels

--- spectra_plot.py
+++ spectra_plot.py
@@ -116,7 +116,7 @@ def spectra_plot(pair):
         i = i + 10
 
     j = 0
-    fig,ax = plt.subplots(2,1, figsize = (20,10))
+    fig,ax = plt.subplots(1,2, figsize = (20,10))
     while j < 1000: 
         temp = []
         temp_uv = []
@@ -130,7 +130,7 @@ def spectra_plot(pair):
  #           print(num)
             f_num_uv = float(num)
             temp_uv.append(f_num_uv)
-        fig,ax = plt.subplots(2,1, figsize = (20,10))
+        fig,ax = plt.subplots(1,2, figsize = (20,10))
         ax[1].plot(sec_wl[:len(temp)], temp[:len(sec_wl)])
         ax[0].plot(sec_wl_uv, temp_uv)
         ax[1].set_xlim(1,5)
